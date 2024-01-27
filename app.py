ec2816a061e18045183b025c5d7f3342016d14fc
amitkumarghosh <professionalakg@gmail.com>
2024-01-27 11:46:54 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import time as t
+
+# with st.spinner("Amit is waiting for you !!!!"):
+#     t.sleep(1)
+# st.balloons()
+st.title("AMIT GHOSHAMIT KUMAR GHOSH")
+st.subheader("ASSISTANT MANAGER - DATA ANALYST, MICROSOFT OFFICE SPECIALIST CERTIFIED")
+st.progress(100)
+st.markdown(":calling:9038191929 /9073377327")
+st.markdown(":email: professionalakg@gmail.com")
+st.markdown(":world_map: Chhutipur, Nanda, Hooghly, West Bengal - 712124")
+st.progress(100)
+st.markdown("###### With over 12 years of hands-on experience in DataWarehousing, Process Validation, and Business NeedsAnalysis, I bring a wealth of expertise to the realm ofBusiness Intelligence. As a seasoned Senior Data Analyst,I am driven by a profound passion for Big Data and anunwavering commitment to transforming rawinformation into Strategic Business Insights. I essentiallybring the following competency landscape of theorganization I serve.")
+st.progress(100)
+st.markdown("#### EXPERIENCE")
+st.markdown("### *************************************")
+st.markdown(":calendar: May2018-Current")
+st.markdown(":office: ASSISTANT MANAGER-DATA ANALYST -- Anudip Foundation for social welfare")
+st.markdown("###### -Prepared automation to calculate daily effortless, low-time-consuming smooth attendance reports by using macro andpower bi. At Anudip, increased attendance percentage from47.31% to 62.27%.")
+st.markdown("###### -Leveraged text, charts and graphs to communicate findings inan understandable format. Use to find the unutilized employee& utilized them as per requirements.")
+st.markdown("###### -Analyzed large amounts of data to identify trends and findpatterns, signals and hidden stories within the data. I help tofind if there in any anomalies.")
+st.markdown("###### -Created and automated data visualizations to present insightsand tell compelling stories by using the looker studio to helpthe employees live performance.")
+st.markdown("###### -Implemented randomized sampling techniques for optimizedsurveys by google form & FLEXIQUIZ.")
+st.markdown("###### -Made hiring recommendations to increase the company'sproductivity and profitability with quality workers. To ensurethe quality employee can deliver the best outcome.")           
+st.markdown("###### -I coached team members in the techniques necessary tocomplete job tasks.")
+st.markdown("###### -Evaluated employee skills and knowledge regularly, training,and mentoring individuals with lagging skill.")             
+
+
+st.markdown(":calendar: May2017-Sep2018")
+st.markdown(":office: MIS EXECUTIVE -- KUTCHINA")
+st.markdown("###### -Produced tables, pivot reports, and charts for use in keydecision-making processes.")
+st.markdown("###### -Planned and led the administration of the management information system for achieving threshold-levelperformance.")
+st.markdown("###### -Tracked different types of metrics daily and organized themfor reporting purposes.")
+st.markdown("###### -Resolved workflow issues to optimize team productivity andimprove delivery times.")
+st.markdown("###### -Designed an internal dashboard for at-a-glance system andproject status reporting.")
+st.markdown("###### -Determined data retention parameters based onorganizational requirements.")
+                                                                       
+st.markdown(":calendar: Jan2013-Apr2017")
+st.markdown(":office: MIS EXECUTIVE -- Jupiter International Ltd. (Frontech)")
+st.markdown("###### -Used ERP-IFS (Enterprise & Distribution module) to input,analyze and model data.")
+st.markdown("###### -Generated reports each month covering post-salesservice transactional data.")
+st.markdown("###### -Aging of outstanding Customer Liabilities")
+st.markdown("###### -Out-of-Warranty Revenue generation report and analysis")
+st.markdown("###### -Product movement report between branches and analysis")
+st.markdown("###### -Sales vs. Expenses Report for pan India Services.")
+st.markdown("###### -Designed various internal dashboard for at-a-glance systemand project status reporting.")
+
+st.markdown(":calendar: Aug2011-Dec2012")
+st.markdown(":office: MIS EXECUTIVE -- DigiCall Teleservices Pvt. Ltd.")
+st.markdown("###### -Preparation & updating of employee dashboard MIS andpresent it in the front of management.")
+st.markdown("###### -Creation & implementation of auto updated Excel-basedman power tracker and submission to management.")
+st.markdown("###### -Daily data allotment for the agents.")          
+st.markdown("###### -Preparing daily report based on agents tele-callingfeedback.")
+st.markdown("###### -Preparing monthly employee performance reports.")
+st.markdown("###### -Assist Team handles to team leader to coordinate.")
+
+st.progress(100)
+st.markdown("#### EDUCATION")
+st.markdown("### *************************************")
+st.markdown(":calendar: 2005-2007")
+st.markdown("##### Diploma in Software technology -- CMC")
+st.markdown(":calendar: 2002-2005")
+st.markdown("##### Bachelor of Arts -- Burdwan University")
+
+st.markdown("#### LINKDIN BADGES")
+st.markdown("### *************************************")
+st.markdown(":sports_medal: Microsoft Power BI")
+st.markdown(":sports_medal: Microsoft Power Point")
+st.markdown(":sports_medal: MY SQL")
+
+# st.header("This is header !!!!!!!")
+# st.markdown("# This is first markdown")
+# st.markdown("## This is 2nd markdown")
+# st.markdown("### This is 3rd markdown")
+# st.markdown(":email:")
+# st.selectbox("Choose your deprtments",["L&D","Accounts","HR","PMO"])
+
+# st.set_page_config(
+#     page_title="Energieiteratie",
+#     layout="wide"
+#     )
+
+# col1, col2, col3 = st.columns([1,1,3])
+
+# # with col1: 
+# #     st.header("Input Data")
+
+# # with col2:
+# #     st.header("Header2")
+
+# with col2:
+#     st.header("AMIT GHOSHAMIT KUMAR GHOSH")
+st.sidebar.image("p3.png")
+st.sidebar.markdown("#### Skills")
+st.sidebar.markdown("### *************************************")
+
+st.sidebar.markdown("###### Microsoft Excel (Advanced proficiency On LinkedIn’s Proficiency Matrix)")
+st.sidebar.progress(90)
+
+st.sidebar.markdown("###### Google Apps: Sheets, Forms etc.")
+st.sidebar.progress(80)
+
+st.sidebar.markdown("###### Power BI")
+st.sidebar.progress(80)
+
+st.sidebar.markdown("###### Python")
+st.sidebar.progress(70)
+
+st.sidebar.markdown("###### Structured Query Language (SQL)")
+st.sidebar.progress(60)
+
+st.sidebar.markdown("#### PROFILE SUMMARY")
+st.sidebar.markdown("### *************************************")
+
+st.sidebar.markdown("#### -Proficient data analyst skilled in leveragingcomprehensive data mastery encompassing advancedanalytics, visualization techniques, and strategicinsights to drive informed decision-making andoptimize business outcomes.")
+st.sidebar.markdown("#### -Skilled in diverse technical proficiencies, leveragingexpertise in data analysis, programming languages,and advanced tools to deliver innovative solutions anddrive operational success.")
+st.sidebar.markdown("#### -Strategically-minded professional adept at aligningbusiness objectives with data-driven insights, fosteringgrowth, and guiding impactful decisions fororganizational success.")
+st.sidebar.markdown("#### -Dynamic business intelligence champion adept atharnessing data-driven insights to empower informeddecision-making, optimize processes, and driveorganizational growth.")
+st.sidebar.markdown("#### -Skilled in insightful decision-making, leveraging dataanalytics and strategic analysis to drive informedbusiness strategies and achieve impactful outcomes.")
+
+st.sidebar.markdown("#### AWARD")
+st.sidebar.markdown("### *************************************")
+st.sidebar.markdown(":trophy: Best employee Awards (2021)--Anudip Foundation")
+st.sidebar.markdown(":trophy: Best Employee (2018) --Kutchina")               
